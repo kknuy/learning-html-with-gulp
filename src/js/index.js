033d68ef255f4392622793414a3e5c8219410bb7
kknuy <y3.0n@yandex.by>
2023-04-08 23:49:16 +0300

    Add modal forms

--- src/js/index.js
+++ src/js/index.js
@@ -15,10 +15,6 @@ $(document).ready(function(){
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
-
-
-
-
   
 
   function toggleSlide(item) {
@@ -33,3 +29,49 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  //Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay,#consultation').fadeIn('slow');
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay,#consultation, #feedback, #order').fadeOut('slow');
+  });
+
+
+  $('.button_catalog').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
+function validate(forms) {
+  $(forms).validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      phone: {
+        required: true,
+        phoneType: 'number',
+
+      },
+      email: {
+        required: true,
+        email: true,
+      },  
+    },
+    messages : {
+      name: "Пожалуйста введите имя",
+      phone: "Пожалуйста введите номер телефона",
+      email: "Пожалуйста введите email",
+    }
+  });
+}
+
+validate('#consultation form');
+validate('#order form');
+validate('#consultation-form');
